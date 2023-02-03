9c51d260584ce91f6789e8e32988090185aa9232
youthdui <124243110+youthdui@users.noreply.github.com>
2023-02-03 14:23:48 +0900

    Update test.java

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -8,5 +8,6 @@ public class test {
 	public static void main() {
 		System.out.println("Hello");
 		System.out.println("It is Too hard to using Git");
+		System.out.println("It is high time to use it!");
 	}
 }
