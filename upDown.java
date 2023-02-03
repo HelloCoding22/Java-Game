6c627562cd1920f40872b72d035fac882fade79f
kwh <kwh871005@gmail.com>
2023-02-03 13:11:51 +0900

    자바 게임 코드 수정임.

--- upDown.java
+++ upDown.java
@@ -23,7 +23,7 @@ public class upDown {
 				System.out.println("업!");
 		} while(guess != answer);
 		
-		System.out.println("축하합니다. 시도 횟수는 " + count + " 번 입니다.");
+		System.out.println("축하합니다. 시도 횟수는 " + count + " 번 입니다!!!!!");
 	}
 
 }
